e9abd721c5ebc7392353acadab55837032e0d8e2
Jeongseung Oh <jeongseung.oh@sjsu.edu>
2022-10-17 14:34:57 -0700

    add account

--- studypool/src/App.js
+++ studypool/src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Navbar/Header';
 import Login from './Pages/Login';
 import SignUp from './Pages/Signup';
 import Catalog from './Pages/Catalog';
+import Account from './Pages/Account';
 import { Route, Routes } from "react-router-dom";
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/Catalog" element={<Catalog />} />
+          <Route path="/Account" element={<Account />} />
         </Routes> 
 
     </div>
