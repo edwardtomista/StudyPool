1245c3a66bc4e3bb08f1c422be6cc3fc16f1191b
edwardtomista <edwardtomista@gmail.com>
2022-11-23 20:35:56 -0800

    Added Dialog Form for Groups

--- studypool/src/Pages/Groups.js
+++ studypool/src/Pages/Groups.js
@@ -8,9 +8,16 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Button } from "@mui/material";
 import { UserContext } from "../UserContext";
 import { backend_url } from "../links";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
 
 export default function Groups() {
     const { user, setUser } = useContext(UserContext);
@@ -71,16 +78,46 @@ export default function Groups() {
         ]);
     };
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+
     return (
         <div className="tables">
             <h1>CS 46A</h1>
-            <Button
-                variant="contained"
-                disabled={!user.id}
-                onClick={() => handleCreate()}
-            >
-                Create
-            </Button>
+                <div>
+                    <Button variant="outlined" onClick={handleClickOpen}>
+                        Create New Group
+                    </Button>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Create Group</DialogTitle>
+                        <DialogContent>
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& > :not(style)': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                                >
+                                <TextField id="outlined-basic" label="Title" variant="outlined" />
+                                <TextField id="outlined-basic" label="Subject" variant="outlined" />
+                            </Box>
+                        </DialogContent>
+                        <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Create</Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
